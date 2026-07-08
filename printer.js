0d6204c35bee5eeb5b1281ea8b6aec20d8c38897
Gilang Raka Ramadhan <gilangrakar@gmail.com>
2026-07-08 19:22:18 +0700

    fix: add payment type

--- printer.js
+++ printer.js
@@ -134,8 +134,8 @@ async function buildReceipt(trx, printerType) {
   t(infoRow("Meja",      trx.table?.name || "-")      + "\n");
   t(infoRow("Pelanggan", trx.cust_name || "-")        + "\n");
 
-  if(trx.payment_method) {
-    t(infoRow("Pay", trx.payment_method || "-")        + "\n");
+  if(trx.payment_type) {
+    t(infoRow("Pay", (trx.payment_type || "-").toUpperCase()) + "\n");
   }
 
   // Label seksi khusus printer atas (FOOD)
